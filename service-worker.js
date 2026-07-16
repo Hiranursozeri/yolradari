e00f05002e9d34e2c85c9fced45540daf4f9a900
Hiranur sozeri <szrhiranur@gmail.com>
2026-07-16 11:17:00 +0300

    PWA destegi eklendi: manifest, service worker ve ikonlar

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'yol-radari-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(names =>
+      Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
